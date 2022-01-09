ae18d51f2cd93cae856a1f9a459742df1d93c8eb
jamesliao <JamesCoding888@hotmail.com>
2022-01-09 21:28:31 +0800

    java

--- spring2/src/main/java/com/study/spring2/Hello.java
+++ spring2/src/main/java/com/study/spring2/Hello.java
@@ -4,8 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
-		System.out.println("HelloWorld");
-		System.out.println("HelloWorld");
+		System.out.println("HelloWorld2");
+		System.out.println("HelloWorld3");
 
 	}
 
