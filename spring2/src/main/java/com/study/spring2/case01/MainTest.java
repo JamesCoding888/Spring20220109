25e6073f0389875f0c72fbf37893754b2a615972
jamesliao <JamesCoding888@hotmail.com>
2022-01-12 13:25:48 +0800

    java

---
+++ spring2/src/main/java/com/study/spring2/case01/MainTest.java
@@ -0,0 +1,11 @@
+package com.study.spring2.case01;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainTest {
+	public static void main(String[] args) {
+//		ApplicationContext act =(ApplicationContext)ClassPathXmlApplicationContext("applicationContext.xml");
+//		act.getBean("Person");
+	}
+}
