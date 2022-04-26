f0a60709742dc903ab729e27bf5ee81ad7584009
jamesliao <JamesCoding888@hotmail.com>
2022-04-26 19:21:01 +0800

    spring20220109

--- spring2/src/main/java/com/study/spring2/case05/PersonDao.java
+++
@@ -1,9 +0,0 @@
-package com.study.spring2.case05;
-
-import java.util.List;
-
-public interface PersonDao {
-	public boolean add(Person person);
-	
-	public List<Person> queryAll();
-}
--- spring2/src/main/java/com/study/spring2/case05/PersonService.java
+++
@@ -1,8 +0,0 @@
-package com.study.spring2.case05;
-
-import java.util.List;
-
-public interface PersonService {
-	boolean append(Person person);
-	List<Person> findAll();
-}
